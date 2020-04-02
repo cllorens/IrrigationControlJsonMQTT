76599a88dea19ca72d1959f6467f657a156e6bb7
cllorens <cdavid@upv.es>
2020-04-03 00:38:22 +0200

    Update irrigation.php

--- web/irrigation.php
+++ web/irrigation.php
@@ -10,16 +10,16 @@
 	$password = "Mqtt2020";                   // set your password
 	$client_id = "irrgation-addon"; // make sure this is unique for connecting to sever - you could use uniqid()
 
-	$mqtt = new phpMQTT($server, $port, $client_id);
+	//$mqtt = new phpMQTT($server, $port, $client_id);
 
 	while(true)
     {
-	if ($mqtt->connect(true, NULL, $username, $password))
+	/*if ($mqtt->connect(true, NULL, $username, $password))
 		{
 		$mqtt->publish("bluerhinos/phpMQTT/examples/publishtest", "Hello World! at " . date("r"), 1);
 		$mqtt->close();
 		}
-	else {echo "Time out!\n";}
+	else {echo "Time out!\n";}*/
 
     sleep(1);
 
