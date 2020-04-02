483f6320accc6fb32de4c0c0ffee6710a25923d5
cllorens <cdavid@upv.es>
2020-04-02 16:52:26 +0200

    kk

--- web/index.php
+++ web/index.php
@@ -1,3 +1,20 @@
 <?php
-phpinfo ();
+	require("phpMQTT.php");
+	echo "Starting...\n";
+	
+	$server = "cdavid.gnd.upv.es";     // change if necessary
+	$port = 8883;                     // change if necessary
+	$username = "MQTT";                   // set your username
+	$password = "Mqtt2020";                   // set your password
+	$client_id = "irrgation-addon"; // make sure this is unique for connecting to sever - you could use uniqid()
+
+	$mqtt = new phpMQTT($server, $port, $client_id);	
+
+	if ($mqtt->connect(true, NULL, $username, $password)) 
+		{
+		$mqtt->publish("bluerhinos/phpMQTT/examples/publishtest", "Hello World! at " . date("r"), 0);
+		$mqtt->close();
+		} 
+	else {echo "Time out!\n";}
+
  ?>
