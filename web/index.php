ddc9359903d8768beeba6fb2cb776d456b8ca24e
cllorens <cdavid@upv.es>
2020-04-02 20:18:09 +0200

    kk

--- web/index.php
+++ web/index.php
@@ -4,7 +4,7 @@
 
 	use \Bluerhinos\phpMQTT as phpMQTT;
 
-	$server = "cdavid.gnd.upv.es";     // change if necessary
+	$server = "cdavidcloud.gnd.upv.es";     // change if necessary
 	$port = 1883;                     // change if necessary
 	$username = "MQTT";                   // set your username
 	$password = "Mqtt2020";                   // set your password
