565da33293c0139af561619b278b64f8f19953f1
cllorens <cdavid@upv.es>
2020-05-05 19:11:01 +0200

    prueba

--- web/irrigation.php
+++ web/irrigation.php
@@ -2,28 +2,22 @@
 	require("phpMQTT.php");
 	echo "Starting...\n";
 
-  use \Bluerhinos\phpMQTT as phpMQTT;
+	use \Bluerhinos\phpMQTT as phpMQTT;
 
 	$server = "cdavidcloud.gnd.upv.es";     // change if necessary
 	$port = 1883;                     // change if necessary
-	$username = "MQTT";                   // set your username
+	$username = "mqtt";                   // set your username
 	$password = "Mqtt2020";                   // set your password
-	$client_id = "irrgation-addon"; // make sure this is unique for connecting to sever - you could use uniqid()
+	$client_id = "irrgation-addon-configure"; // make sure this is unique for connecting to sever - you could use uniqid()
 
-	//$mqtt = new phpMQTT($server, $port, $client_id);
+	$mqtt = new phpMQTT($server, $port, $client_id);
 
-	while(true)
-    {
-	/*if ($mqtt->connect(true, NULL, $username, $password))
+	if ($mqtt->connect(true, NULL, $username, $password))
 		{
-		$mqtt->publish("bluerhinos/phpMQTT/examples/publishtest", "Hello World! at " . date("r"), 1);
+		$mqtt->publish("TEST", "Hello World! at " . date("r"), 1);
 		$mqtt->close();
 		}
-	else {echo "Time out!\n";}*/
-
-    sleep(1);
-
-    }
+	else {echo "Time out!\n";}
 
 
  ?>
